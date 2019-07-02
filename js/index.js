e2e520e8967b4eb0a3ab5c6731b1cfddb2bdf146
Vincent Humeau <vincent.humeau@gmail.com>
2019-07-02 13:32:52 +0100

    Updates

--- js/index.js
+++ js/index.js
@@ -91,9 +91,9 @@
   !*** ./Static/sass/_outletsW/_screen.scss ***!
   \********************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\\n\\r\\n}\\r\\n^\\r\\n      Invalid CSS after '...: 600px\\\"></div>': expected 1 selector or at-rule, was '<section class=\\\"fil'\\r\\n      in C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\Static\\\\sass\\\\_outletsW\\\\organisms\\\\_filters.scss (line 124, column 2)\\n    at runLoaders (C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:302:20)\\n    at C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.render [as callback] (C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\sass-loader\\\\lib\\\\loader.js:52:13)\\n    at Object.done [as callback] (C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\neo-async\\\\async.js:8067:18)\\n    at options.error (C:\\\\Users\\\\Vince\\\\Documents\\\\projects\\\\McArthurGlenWeb\\\\McArthurGlen.Web.Ui\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./Static/sass/_outletsW/_screen.scss?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./Static/sass/_outletsW/_screen.scss?");
 
 /***/ }),
 
